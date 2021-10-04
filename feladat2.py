9e7fedb50994639b9befcd65747b674bc39cddba
PBence <90601408+PBencee@users.noreply.github.com>
2021-10-04 10:35:06 +0200

    Add files via upload

---
+++ feladat2.py
@@ -0,0 +1,54 @@
+print ('Feladat 1')
+nev = input ('Neved =')
+print ('Szia',nev)
+
+print ('Feladat 2')
+
+print('**********')
+print('oooooooooo')
+
+print ('Feladat 3')
+
+print('*o*o*o*o*o*')
+print('*o*o*o*o*o*')
+
+print ('Feladat 4')
+
+szam1 = int (input('Szam1 ='))
+szam2 = int (input('Szam2 ='))
+print ('Összege:',szam1 + szam2)
+
+print ('Feladat 5')
+
+szam1 = int (input('Szam1 ='))
+szam2 = int (input('Szam2 ='))
+szam3 = int (input('Szam3 ='))
+print ('Összege:',szam1 + szam2 + szam3)
+
+print ('Feladat 6')
+
+szam1 = int (input('Szam1 ='))
+szam2 = int (input('Szam2 ='))
+print ('Összege:',szam1 - szam2)
+
+print ('Feladat 7')
+
+szam1 = int (input('Szam1 ='))
+szam2 = int (input('Szam2 ='))
+print ('Összege:',szam1 * szam2)
+
+print ('Feladat 8')
+
+szam1 = int (input('Szam1 ='))
+szam2 = int (input('Szam2 ='))
+print ('Összege:',szam1 / szam2)
+
+
+
+
+
+
+
+
+
+
